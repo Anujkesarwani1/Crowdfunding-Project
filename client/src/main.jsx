4a0a6809bf8acdf6900ba2cb5cce540c8c83e8fd
Anuj Kesarwani <anuj.kesarwani@zemosolabs.com>
2023-05-01 20:41:18 +0530

    setup react app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import './index.css';
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </ThirdwebProvider>
 );
---
+++ client/src/pages/CreateCampaign.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateCampaign = () => {
+  return (
+    <div>
+      create campaign
+    </div>
+  )
+}
+
+export default CreateCampaign
